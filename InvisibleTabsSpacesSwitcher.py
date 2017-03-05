10bc978471d0bd6fd64a6f43a4ee0d606065b9e4
isar0se <itsr053@gmail.com>
2017-03-05 18:41:08 -0500

    rewrite

--- InvisibleTabsSpacesSwitcher.py
+++ InvisibleTabsSpacesSwitcher.py
@@ -2,19 +2,37 @@
 
 import sublime, sublime_plugin # modules provided by Sublime Text
 
-class InvisibleTabsSpacesSwitcher(sublime_plugin.EventListener):
+# hook for opening files when ST is closed (b/c files load first, then plugins)
+def plugin_loaded():
+  if sublime.load_settings('Preferences.sublime-settings').get('invisible_tabs_spaces_switching') == True:
+    for view in sublime.active_window().views(): # loop through opened files
+      load_file(view)
+
+# loading & closing methods for the plugin to use
+def load_file(view):
+  if view.file_name(): # ignore untitled default/background files
+    if not view.file_name().endswith('.sublime-settings') and not view.file_name().endswith('.sublime-keymap'): # ignore preferences and key-bindings files
+      if view.settings().get('view_files_with_tabs') == True:
+        view.run_command('unexpand_tabs') # convert to tabs
+      else: # aka, we want to view files with spaces
+        view.run_command('expand_tabs') # convert to spaces
+      view.run_command('save')
+  
+def close_file(view):
+  if view.file_name():
+    if not view.file_name().endswith('.sublime-settings') and not view.file_name().endswith('.sublime-keymap'):
+      if view.settings().get('view_files_with_tabs') == True:
+        view.run_command('expand_tabs')
+      else: # aka, we want to save files with tabs
+        view.run_command('unexpand_tabs')
+      view.run_command('save')
+
+# the plugin itself
+class InvisibleTabsSpacesSwitcherCommand(sublime_plugin.EventListener):
   def on_load(self, view): # event triggered when file is loaded
     if view.settings().get('invisible_tabs_spaces_switching') == True:
-      if view.settings().get('view_files_with_tabs') == True:
-        view.run_command('unexpand_tabs')
-      else: # aka, we want to view files with spaces
-        view.run_command('expand_tabs')
+      load_file(view)
 
   def on_pre_close(self, view): # event triggered before file closes
     if view.settings().get('invisible_tabs_spaces_switching') == True:
-      if view.file_name(): # avoid untitled dummy/background files
-        if view.settings().get('view_files_with_tabs') == True:
-          view.run_command('expand_tabs')
-        else: # aka, we want to save files with tabs
-          view.run_command('unexpand_tabs')
-        view.run_command('save')
+      close_file(view)
